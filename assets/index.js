43ad397698420b6eb084646c2211fc12856393f5
Maldrek <scott.salem5@gmail.com>
2018-04-24 20:22:44 -0500

    added more styling elements and set up for javascript and additional pages

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,3 @@
-jQuery(function(){ "use strict";
-        jQuery(".helloWorld").typed({
-          strings: ["مرحبا بالعالم", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "你好，世界", "Hallo Welt", "こんにちは世界","Hello world"],
-          typeSpeed: 30, // typing speed
-          backDelay: 500, // pause before backspacing
-          loop: true, // loop on or off (true or false)
-          loopCount: false, // number of loops, false = infinite
-        });
-		return false;
-    });
+let introArray = ["مرحبا بالعالم", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "你好，世界", "Hallo Welt", "こんにちは世界","Hello world"];
+
+$(".hellWorld").push(introArray[7]).fadeIn(1500).delay(2000).fadeOut(1500);
