6f3a4707a9d27aebae587325ec19f42165d423ce
Maldrek <scott.salem5@gmail.com>
2018-04-25 18:32:43 -0500

    trying to figure out typing function

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
 
-let typed = require("typed.js");
+// taken from https://github.com/mattboldt/typed.js/
+// let typed = require("typed.js");
 
-let introArray = ["Hello world","مرحبا بالعالم", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "你好，世界", "Hallo Welt", "こんにちは世界"];
+// // let introArray = ["Hello world","مرحبا بالعالم", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "你好，世界", "Hallo Welt", "こんにちは世界"];
 
+// import Typed from 'typed.js';
 
+// var options = {
+//     strings: ["Hello world","مرحبا بالعالم", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "你好，世界", "Hallo Welt", "こんにちは世界"],
+//     typeSpeed: 40
+//   }
+
+// let typed = new Typed("#intro", options);
 
 $(".nav1").on("click", function() {
     document.querySelector("#about").scrollIntoView({
