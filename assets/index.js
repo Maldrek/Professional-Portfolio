b66b9548bd42262aef203074ad240494bff53e93
Maldrek <scott.salem5@gmail.com>
2018-04-26 17:51:46 -0500

    trying to get typeit CDN to work properly

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,14 @@
-$(document).ready(function () {
+// strings: ["Hello world", "مرحبا بالعالم", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "你好，世界", "Hallo Welt", "こんにちは世界"],
 
-    let typing = new TypeIt('#typing', {
-        speed: 750,
-        lifeLike: true,
-        autoStart: true,
-        cursor: true,
-        loop: true,
-        strings: ["Hello world", "مرحبا بالعالم", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "你好，世界", "Hallo Welt", "こんにちは世界"],
-        breakLines: false
-    });
-
-    typing.init();
+new TypeIt('#typing', {
+    speed: 750,
+    lifeLike: true,
+    autoStart: true,
+    cursor: true,
+    loop: true,
+    strings: ["Hello world", "مرحبا بالعالم", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "你好，世界", "Hallo Welt", "こんにちは世界"],
+    breakLines: false
 });
-
-let introArray = ["Hello world", "مرحبا بالعالم", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "你好，世界", "Hallo Welt", "こんにちは世界"];
 
 $(".nav1").on("click", function () {
     document.querySelector("#about").scrollIntoView({
