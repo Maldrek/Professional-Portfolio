90d5e2b8c0f2d513c8eb24b4bddf41a9f5da3167
Maldrek <scott.salem5@gmail.com>
2018-04-25 20:35:07 -0500

    trying to get typeit to work

--- assets/index.js
+++ assets/index.js
@@ -1,32 +1,45 @@
-let typeIt = require("typeit");
+$(document).ready(function () {
 
-let introArray = ["Hello world","مرحبا بالعالم", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "你好，世界", "Hallo Welt", "こんにちは世界"];
+    let typing = new TypeIt('#typing', {
+        speed: 750,
+        lifeLike: true,
+        autoStart: true,
+        cursor: true,
+        loop: true,
+        strings: ["Hello world", "مرحبا بالعالم", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "你好，世界", "Hallo Welt", "こんにちは世界"],
+        breakLines: false
+    });
 
-$(".nav1").on("click", function() {
+    typing.init();
+});
+
+let introArray = ["Hello world", "مرحبا بالعالم", "Hola Mundo", "Bonjour le monde", "Ciao mondo", "你好，世界", "Hallo Welt", "こんにちは世界"];
+
+$(".nav1").on("click", function () {
     document.querySelector("#about").scrollIntoView({
         behavior: "smooth"
     });
 });
 
-$(".nav2").on("click", function() {
+$(".nav2").on("click", function () {
     document.querySelector("#portfolio").scrollIntoView({
         behavior: "smooth"
     });
 });
 
-$(".nav3").on("click", function() {
+$(".nav3").on("click", function () {
     document.querySelector("#resume").scrollIntoView({
         behavior: "smooth"
     });
 });
 
-$(".nav4").on("click", function() {
+$(".nav4").on("click", function () {
     document.querySelector("#contact").scrollIntoView({
         behavior: "smooth"
     });
 });
 
-$("#nav-headshot").on("click", function() {
+$("#nav-headshot").on("click", function () {
     document.querySelector("#landing").scrollIntoView({
         behavior: "smooth"
     });
